b6b109b6a8267e81dea5bb8335592f34e0502d0e
Petulant Slacker <l.goohsen@gmail.com>
2017-06-09 17:23:23 +0200

    added solution for books

--- htw-scala/src/de/htw/fp/assignment6/books.scala
+++ htw-scala/src/de/htw/fp/assignment6/books.scala
@@ -4,13 +4,23 @@ object books {
   def main(args: Array[String]) = {
     val books = List(
         Book("Scala Cookbook", List("A", "B", "C")),
-        Book("sdfsdf", List("B"))
+        Book("Scala for the impatient", List("Cay Horstmann")),
+        Book("Core Java for the impatient", List("Cay Horstmann")),
+        Book("sdfsdf", List("B", "C"))
     )
+    println(findAuthorsWithMultipleTitles(books))
   }
   
   case class Book(title: String, authors: List[String])
   
   def findAuthorsWithMultipleTitles(books: List[Book]): List[String] = {
-    List()
+      (for {
+        book1 <- books
+        book2 <- books
+        if book1 != book2
+        author1 <- book1.authors
+        author2 <- book2.authors
+        if author1 == author2
+      } yield author1) distinct
   }
 }
