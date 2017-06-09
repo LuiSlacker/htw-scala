ad6bf3a0782889311e509911231cc9bf17def609
Gregor <gdur.mugen@gmail.com>
2017-06-09 21:26:51 +0200

    coins_with_solutionexport.scala ist nun komplett funktional programmiert assertions angepasst

--- htw-scala/src/de/htw/fp/assignment6/coins_with_solutionexport.scala
+++ htw-scala/src/de/htw/fp/assignment6/coins_with_solutionexport.scala
@@ -3,54 +3,31 @@ package de.htw.fp.assignment6
 import scala.collection.mutable.Stack
 
 object coins_with_solutionexport extends App {
-  //def münzStückelungen (werte: List[Int], betrag: Int): List[List[Int]]
 
-  def sumup(l: List[Int]): Int = {
+  def sumUp(l: List[Int]): Int = {
     if (l.isEmpty) 0
-    else sumup(l.tail) + l.head
+    else sumUp(l.tail) + l.head
   }
-  
-  def countPricePossibilities2(goal: Int, l: List[Int]): List[List[Int]] = {
+
+  def countPricePossibilities(goal: Int, coins: List[Int]): List[List[Int]] = {
 
     def checkPrice(sumList: List[Int], l: List[Int], solutions: List[List[Int]]): List[List[Int]] = {
-      val sum = sumup(sumList)
-      //      println(sum, sumList)
-      if (goal == 0) List(Nil)
-      else if (l.isEmpty) solutions
+      // we could use a sum:Int variable and thus avoid the sumUp method
+      val sum = sumUp(sumList)
+
+      if (l.isEmpty) solutions
       else if (goal == sum) sumList :: solutions
       else if (sum > goal) checkPrice(sumList, l.tail, solutions)
       else if (sum < goal) checkPrice(l.head :: sumList, l, solutions) ::: checkPrice(sumList, l.tail, solutions)
+      else Nil
 
-      Nil
     }
-    var t = checkPrice(List(), l, List[List[Int]]())
-
-    println(t)
-    t
+    
+    if (coins.isEmpty)
+      List(List())
+    else
+      checkPrice(List(), coins, List[List[Int]]())
   }
-    def countPricePossibilities(goal: Int, l: List[Int]): List[List[Int]] = {
-      var solutions = List[List[Int]]()
-  
-      if (goal == 0 || l.isEmpty) {
-        List(List())
-        1
-      }
-  
-      def checkPrice(sumList: List[Int], l: List[Int]): Int = {
-        val sum = sumup(sumList)
-        if (l.isEmpty) 0
-        else if (goal == sum) {
-          solutions ++= List(sumList)
-          1
-        } else if (sum > goal) checkPrice(sumList, l.tail)
-        else if (sum < goal) checkPrice(l.head :: sumList, l) + checkPrice(sumList, l.tail)
-        0
-      }
-      checkPrice(List(), l)
-  
-      solutions
-  
-    }
 }
 
    
--- htw-scala/src/de/htw/fp/assignment6/main.scala
+++ htw-scala/src/de/htw/fp/assignment6/main.scala
@@ -4,33 +4,31 @@ object test {
   def main(args: Array[String]) = {
 
     def runAssertionsCoins(coinOptions: (Int, List[Int]) => List[List[Int]]) = {
-      // countCharFrequencies()
 
       val coinVersions = List(5, 2, 3)
 
       println("")
-      println("checkPrice:")
+      println("retrieve coinlist solutions:")
 
-      assert(coinOptions(0, List()), List())
-      assert(coinOptions(0, List(0)), List())
-      assert(coinOptions(0, coinVersions), List())
+      assert(coinOptions(0, List()), List(List()))
+      assert(coinOptions(0, List(0)), List(List()))
+      assert(coinOptions(0, coinVersions), List(List()))
       assert(coinOptions(1, coinVersions), List())
       assert(coinOptions(2, coinVersions), List(List(2)))
-      assert(coinOptions(3, coinVersions), 1)
-      assert(coinOptions(4, coinVersions), 1)
-      assert(coinOptions(5, coinVersions), List(List(5), List(2, 3)))
-      assert(coinOptions(6, coinVersions), 2)
-      assert(coinOptions(7, coinVersions), 2)
-      assert(coinOptions(8, coinVersions), 3)
-      assert(coinOptions(9, coinVersions), 3)
-      //    assert(coinOptions(10, coinVersions), 4)
-      //    assert(coinOptions(11, coinVersions), 4)
-      //    assert(coinOptions(12, coinVersions), 5)
+      assert(coinOptions(3, coinVersions), List(List(3)))
+      assert(coinOptions(4, coinVersions), List(List(2, 2)))
+      assert(coinOptions(5, coinVersions), List(List(5), List(3, 2)))
+      assert(coinOptions(6, coinVersions), List(List(2, 2, 2), List(3, 3)))
+      assert(coinOptions(7, coinVersions), List(List(2, 5), List(3, 2, 2)))
+      assert(coinOptions(8, coinVersions), List(List(3, 5), List(2, 2, 2, 2), List(3, 3, 2)))
+      assert(coinOptions(9, coinVersions), List(List(2, 2, 5), List(3, 2, 2, 2), List(3, 3, 3)))
+      assert(coinOptions(10, coinVersions), List(List(5, 5), List(3, 2, 5), List(2, 2, 2, 2, 2), List(3, 3, 2, 2)))
+      assert(coinOptions(11, coinVersions), List(List(2, 2, 2, 5), List(3, 3, 5), List(3, 2, 2, 2, 2), List(3, 3, 3, 2)))
+      assert(coinOptions(12, coinVersions), List(List(2, 5, 5), List(3, 2, 2, 5), List(2, 2, 2, 2, 2, 2), List(3, 3, 2, 2, 2), List(3, 3, 3, 3)))
 
     }
 
-    runAssertionsCoins(coins_with_solutionexport.countPricePossibilities2)
-    //    runAssertionsCoins(coins_with_solutionexport.countPricePossibilities)
+    runAssertionsCoins(coins_with_solutionexport.countPricePossibilities)
 
     def assert(a: Any, b: Any) =
       if (a == b) println("Jupp " + a + " == " + b)
