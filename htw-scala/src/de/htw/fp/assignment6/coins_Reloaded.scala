9e8fb09e60d510a9b16459fac7ebc88abd9208ce
Petulant Slacker <l.goohsen@gmail.com>
2017-06-09 16:26:12 +0200

    added coin denomintaion with lists

--- htw-scala/src/de/htw/fp/assignment6/coins_Reloaded.scala
+++ htw-scala/src/de/htw/fp/assignment6/coins_Reloaded.scala
@@ -3,21 +3,23 @@ package de.htw.fp.assignment6
 
 object coins_Reloaded {
   def main(args: Array[String]) {
-    println(coinOptions(3, List(1, 2, 5)))
+    println(coinOptions(9, List(1, 2, 5)))
   }
 
   /**
-   * returns number of possible denominations for a given price
+   * returns a List of possible denominations for a given price
    * @param price the price to achieve
    * @param coinDenominations the list of possible coins
    */
-  def coinOptions(price: Int, coinDenominations: List[Int]): Int = {
-    def count(price: Int, coinDenominations: List[Int], counter: Int, coins:List[Int]): Int = {
-      if (price == 0) counter + 1
-      else if (price < 0 || coinDenominations.isEmpty) counter
-      else count(price - coinDenominations.head, coinDenominations, counter, coinDenominations.head :: coins) + count(price, coinDenominations.tail, counter, coins)
+  def coinOptions(price: Int, coinDenominations: List[Int]): List[List[Int]] = {
+    def count(price: Int, coinDenominations: List[Int], coinsTaken: List[Int], possibilities:List[List[Int]]): List[List[Int]] = {
+      if (price == 0) coinsTaken :: possibilities
+      else if (price < 0 || coinDenominations.isEmpty) possibilities
+      else
+        count(price - coinDenominations.head, coinDenominations, coinDenominations.head :: coinsTaken, possibilities) :::
+        count(price, coinDenominations.tail, coinsTaken, possibilities)
     }
 
-    count(price, coinDenominations, 0, List())
+    count(price, coinDenominations, List(), List())
   }
 }
