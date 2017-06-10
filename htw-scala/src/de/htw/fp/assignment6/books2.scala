c5a0304138486d43fda36a27a4dd860feb8fa3f1
Gregor <gdur.mugen@gmail.com>
2017-06-10 15:17:13 +0200

    books2.scala is working

---
+++ htw-scala/src/de/htw/fp/assignment6/books2.scala
@@ -0,0 +1,39 @@
+package de.htw.fp.assignment6
+
+object books2 {
+  def main(args: Array[String]) = {
+
+    val books = List(
+      new Book("The Martian", List("Andy Weir")),
+      new Book("Harry Potter", List("J. K. Rolling")),
+      new Book("Bad Book 1", List("Andy Weir")),
+      new Book("Bad Book 2", List("Andy Weir", "Peter Pan")),
+      new Book("And The Cow Makes Moo", List("J. K. Rolling", "Andy Weir")),
+      new Book("The Lake", List("Peter Gross")))
+
+    var found = findAuthorsWithMultipleTitles(books)
+    var expected = List("J. K. Rolling", "Andy Weir")
+
+    println(found)
+    println(expected)
+    println(found == expected)
+
+  }
+
+  case class Book(title: String, authors: List[String])
+
+  def findAuthorsWithMultipleTitles(books: List[Book]): List[String] = {
+
+    var res = books.map(book => {
+      book.authors.map(author => {
+        author
+      })
+    }).flatten
+      .groupBy(x => x)
+      .filter((t) => t._2.length > 1)
+      .keys
+      .toList
+
+    res
+  }
+}
